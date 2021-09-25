a643b73e15b909ad19c80ed260afcaa351712086
StacieKot <76011979+StacieKot@users.noreply.github.com>
2021-09-25 18:26:24 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const devServer = (isDev) =>
+  !isDev
+    ? {}
+    : {
+        devServer: {
+          hot: true,
+          open: 'chrome',
+          historyApiFallback: true,
+          contentBase: path.join(__dirname, 'public'),
+        },
+      };
+
+module.exports = ({ development }) => ({
+  mode: development ? 'development' : 'production',
+  target: 'web',
+  devtool: development ? 'cheap-module-source-map' : 'source-map',
+  entry: {
+    main: path.resolve(__dirname, '..', './src/index.tsx'),
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, '..', './build'),
+    assetModuleFilename: 'assets/[hash][ext]',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, '..', './public'), to: 'public' },
+      ],
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+      failOnError: true,
+    }),
+  ],
+  ...devServer(development),
+});
